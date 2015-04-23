eed356251b53e3cfc607f9665632f8aea7b664e6
Tomi Pieviläinen <tomi.pievilainen@iki.fi>
2015-04-23 10:34:12 +0300

    Force parameter for import

--- import-data.sh
+++ import-data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ -n "$1" ]]; then
+    if [[ "$1" = "--force" ]]; then
+        FORCE=1
+    else
+        echo "Unknown argument $1"
+    fi
+fi
+
 echo "Creating data directory"
 mkdir -p /data/elasticsearch
 
@@ -9,28 +17,28 @@ echo "Creating index"
 ./create_index.py
 
 echo "Updating address data..."
-if [[ "$(curl -z /data/osoitteet.csv --retry 5 -f http://ptp.hel.fi/avoindata/aineistot/Paakaupunkiseudun_osoiteluettelo.zip -o osoitteet.zip -s -L -w %{http_code})" == "200" ]]; then
-    unzip -jDD osoitteet.zip &&
-    mv PKS_avoin_osoiteluettelo.csv /data/osoitteet.csv &&
-    rm osoitteet.zip *_kuvaus.pdf &&
-    echo "Processing address data" &&
+if [[ "$(curl -z /data/osoitteet.csv --retry 5 -f http://ptp.hel.fi/avoindata/aineistot/Paakaupunkiseudun_osoiteluettelo.zip -o osoitteet.zip -s -L -w %{http_code})" == "200" ]] &&
+      unzip -jDD osoitteet.zip &&
+      mv PKS_avoin_osoiteluettelo.csv /data/osoitteet.csv &&
+      rm osoitteet.zip *_kuvaus.pdf || [[ $FORCE ]]; then
+    echo "Processing address data"
     ./addresses.py /data/osoitteet.csv
 else
     echo -e "\tNo new data available"
 fi
 
 echo "Updating municipal data..."
-if [[ "$(curl -z /data/kuntajako.xml --retry 5 -f http://kartat.kapsi.fi/files/kuntajako/kuntajako_10k/etrs89/gml/TietoaKuntajaosta_2015_10k.zip -o kuntajako.zip -s -L -w %{http_code})" == "200" ]]; then
-    unzip -jDD kuntajako.zip TietoaKuntajaosta_2015_10k/SuomenKuntajako_2015_10k.xml &&
-    mv SuomenKuntajako_2015_10k.xml /data/kuntajako.xml &&
-    rm kuntajako.zip &&
+if [[ "$(curl -z /data/kuntajako.xml --retry 5 -f http://kartat.kapsi.fi/files/kuntajako/kuntajako_10k/etrs89/gml/TietoaKuntajaosta_2015_10k.zip -o kuntajako.zip -s -L -w %{http_code})" == "200" ]] &&
+      unzip -jDD kuntajako.zip TietoaKuntajaosta_2015_10k/SuomenKuntajako_2015_10k.xml &&
+      mv SuomenKuntajako_2015_10k.xml /data/kuntajako.xml &&
+      rm kuntajako.zip || [[ $FORCE ]]; then
     ./mml_municipalities.py /data/kuntajako.xml
 else
     echo -e "\tNo new data available"
 fi
 
 echo "Updating OpenStreetMap data..."
-if [[ "$(curl -z /data/finland-latest.osm.pbf --retry 5 -f http://download.geofabrik.de/europe/finland-latest.osm.pbf -o /data/finland-latest.osm.pbf -s -L -w %{http_code})" == "200" ]]; then
+if [[ "$(curl -z /data/finland-latest.osm.pbf --retry 5 -f http://download.geofabrik.de/europe/finland-latest.osm.pbf -o /data/finland-latest.osm.pbf -s -L -w %{http_code})" == "200" || $FORCE ]]; then
     echo "Processing OpenStreetMap data"
     ./osm_pbf.py /data/finland-latest.osm.pbf /data/kuntajako.xml
 else
@@ -51,10 +59,10 @@ echo "Processing lipas data" &&
 ./lipas.py /data/lipas_kaikki_pisteet
 
 echo "Updating GTFS data..."
-if [[ "$(curl -z /data/stops.txt --retry 5 -f http://matka.hsl.fi/route-server/hsl.zip -o gtfs.zip -s -L -w %{http_code})" == "200" ]]; then
-    unzip -DD gtfs.zip stops.txt &&
-    mv stops.txt /data/ &&
-    rm gtfs.zip &&
+if [[ "$(curl -z /data/stops.txt --retry 5 -f http://matka.hsl.fi/route-server/hsl.zip -o gtfs.zip -s -L -w %{http_code})" == "200" ]] &&
+      unzip -DD gtfs.zip stops.txt &&
+      mv stops.txt /data/ &&
+      rm gtfs.zip || [[ $FORCE ]]; then
     echo "Processing GTFS data" &&
     ./stops.py /data/stops.txt
 else
