77885579a7021aeb0ab884e87af261060e4ae0aa
Tomi Pieviläinen <tomi.pievilainen@iki.fi>
2015-05-19 09:29:28 +0300

    Remove debug output from script

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -4,4 +4,4 @@ export LANG=C.UTF-8
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 $DIR/elastic-wait.sh
 echo "Starting API webserver"
-app -p $1 -vv -d `date '+%F' --date="$(stat -c '@%Y' /data/updated)"` --docs $DIR/../docs/_build/html/
+app -p $1 -v -d `date '+%F' --date="$(stat -c '@%Y' /data/updated)"` --docs $DIR/../docs/_build/html/
